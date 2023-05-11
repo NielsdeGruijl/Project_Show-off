0ce4220d21c13b3707840ebc542c752a0645c8d6
Niels de Gruijl <niels07@outlook.com>
2023-05-11 15:15:05 +0200

    Created a way to reduce boat bobbing

---
+++ ShowOffProject/Source/ShowOffProject/PlayerPawn.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class SHOWOFFPROJECT_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	float minRot = -1, maxRot = 1;
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+	FVector ClampRotation();
+
+	//UPROPERTY(EditAnywhere)
+	//FVector deltaRotation;
+};
